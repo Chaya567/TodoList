763b57567411377f187f7abbafda34bea3f3d2ec
Chaya567 <chaia.fogel@gmail.com>
2024-04-30 12:27:37 +0300

    Added a new component and made changes to App.js and CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
+import React from 'react';
 import './App.css';
 import { TodoWrapper } from './components/TodoWrapper';
+import WeatherComponent from './components/Weather'; 
 
 function App() {
   return (
     <div className="App">
       <TodoWrapper />
+      <WeatherComponent /> {/* הוסף את הקומפוננטה של מזג האוויר */}
     </div>
   );
 }
